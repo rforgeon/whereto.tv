a7d5a27205bc9badd7c7b056a255b79a1f45167e
rforgeon <rforgeon@gmail.com>
2016-11-04 15:40:37 -0700

    post validate completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :place
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :link, length: { minimum: 10 }, presence: true
+  validates :place, presence: true
+
 end
